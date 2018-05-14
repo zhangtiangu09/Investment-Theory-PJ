7b9971d134ac6daeb5afe7ff0b887b34338d398d
IvyKnight <39109973+zhangtiangu09@users.noreply.github.com>
2018-05-14 11:39:45 -0400

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -37,3 +37,33 @@ def download_data(ticker,freq="1d"):
 if __name__ == "__main__":
     ticker = ["FXE","EWJ","GLD","QQQ","SPY","SHV","DBA","USO","XBI","ILF","GAF","EPP","FEZ"]
     download_data(ticker)
+    path = os.getcwd()+"/data"
+
+    # initilize dataframe by benchmark
+    data = pd.read_csv(path+"/SPY.csv")
+    data = data.loc[:,["Adj Close","Date"]]
+    data.columns=["SPY","Date"]
+
+    #get all adj_close and inner join
+    ticker = ["FXE","EWJ","GLD","QQQ","SHV","DBA","USO","XBI","ILF","GAF","EPP","FEZ"]
+
+    for i in ticker:
+        adj_close = pd.read_csv(path+"/{}.csv".format(i))
+        adj_close = adj_close.loc[:,["Adj Close","Date"]]
+        adj_close.columns=[i,"Date"]
+        data = pd.merge(data,adj_close,how="inner",on="Date")
+    
+    data.Date = pd.to_datetime(data.Date)
+    #import factors's data
+    factors = pd.read_csv(path+"/F-F_Research_Data_Factors_daily.CSV",
+                      skiprows=5,
+                      header=None,
+                      names=["Date","Mkt-RF","SMB","HML","RF"])
+    factors = factors.iloc[:-1,:]
+    factors.Date = pd.to_datetime(factors["Date"],format="%Y%m%d")
+    data = pd.merge(data,factors,how="inner",on="Date")
+
+    data.set_index("Date",inplace=True)
+    data = data.drop(data.index[data["GAF"]=="null"])
+    data = data.applymap(float)
+    data.to_csv(os.getcwd()+"/data/adj_close.csv")
